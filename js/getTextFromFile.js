618164cfbfd124b84c81f718ddcc639ced54b5de
scemama <scemama@users.noreply.github.com>
2020-11-24 18:21:58 +0000

    deploy: daef56c12e20a7ea1a1fe501795c62111ed5a987

---
+++ js/getTextFromFile.js
@@ -0,0 +1,53 @@
+var fileCache = function () {
+  var json_url = "/data/database.json";
+  var req = new XMLHttpRequest();
+  req.open("GET",json_url, false);
+  req.send();
+  return JSON.parse(req.responseText);
+}
+
+
+_cache = fileCache();
+function getTextFromFileUrl(url,header={}) {
+  return _cache[url];
+}
+
+
+async function getTextFromFileUrlAsync(url,header={}) {
+  return new Promise(function (resolve, reject) {
+  var req = new XMLHttpRequest();
+  req.open("GET",url, true);
+  for (const [key, val] of Object.entries(header)) {
+    req.setRequestHeader(key,val)
+  }
+  req.onreadystatechange = function() {
+    if (req.readyState == 4) {
+      if (req.status == 200 || req.status==304) {//when a good response is given do this
+        var text = req.responseText;
+        resolve(text);
+      } else {
+        reject({
+          status: req.status,
+          statusText: req.statusText
+        });
+      }
+    }
+  }
+  req.send();
+  });
+}
+async function getTextFromUploadedFileAsync(inputFile){
+  const temporaryFileReader = new FileReader();
+
+  return new Promise((resolve, reject) => {
+    temporaryFileReader.onerror = () => {
+      temporaryFileReader.abort();
+      reject(new DOMException("Problem parsing input file."));
+    };
+
+    temporaryFileReader.onload = () => {
+      resolve(temporaryFileReader.result);
+    };
+    temporaryFileReader.readAsText(inputFile);
+  });
+};
